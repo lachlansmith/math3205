2ff6f6b1e8f954a8a2caca63e679a6f1d5840393
lachlansmith <lachysmith@me.com>
2023-09-16 12:58:07 +1000

    exception

--- binpacking/__init__.py
+++ binpacking/__init__.py
@@ -1,6 +1,7 @@
-from .parse import Parser
-from .model import Bin, Item, Solution
-from .preprocess import Preprocessor
-from .solve import Solver
-from .plot import plot_solution
-from .subproblem import SubproblemSolver
+from .parse import *
+from .model import *
+from .preprocess import *
+from .solve import *
+from .plot import *
+from .subproblem import *
+from .exception import *
--- binpacking/solve.py
+++ binpacking/solve.py
@@ -1,7 +1,6 @@
 from gurobipy import *
-import math
-
-from binpacking.model import Bin, Item, Solution
+from binpacking.model import Bin, Item
+from binpacking.exception import NonOptimalException
 
 
 class Solver:
@@ -35,9 +34,6 @@ class Solver:
     def solve(self) -> list[Bin]:
         """Here we solve the problem using Gurobi."""
 
-        T = range(len(self.items))
-
-        lb = int(math.ceil(sum([self.items[t].area for t in T]) / self.area))
         ub = len(self.items)
 
         env = Env(empty=True)
@@ -47,7 +43,7 @@ class Solver:
         m = Model(env=env)
 
         # item t is assigned to bin b
-        X = {(b, t): m.addVar(vtype=GRB.BINARY) for t in T for b in range(ub)}
+        X = {(b, t): m.addVar(vtype=GRB.BINARY) for t in range(ub) for b in range(ub)}
 
         # bin b is open
         Y = {b: m.addVar(vtype=GRB.BINARY) for b in range(ub)}
@@ -56,10 +52,10 @@ class Solver:
 
         EachItemUsedOnce = {
             t: m.addConstr(quicksum(X[b, t] for b in range(ub)) == 1)
-            for t in T}
+            for t in range(ub)}
 
         SumOfAreasLessThanBinArea = {
-            b: m.addConstr(quicksum(self.items[t].area * X[b, t] for t in T) <= self.area * Y[b])
+            b: m.addConstr(quicksum(self.items[t].area * X[b, t] for t in range(ub)) <= self.area * Y[b])
             for b in range(ub)}
 
         PreviousBinOpen = {
@@ -72,21 +68,15 @@ class Solver:
         bins = []
 
         if m.status == GRB.OPTIMAL:
-            # Count the number of bins used (same as the objective value)
-            print("Number of bins used:", int(m.ObjVal))
-
             for b in range(ub):
                 if Y[b].x > 0.5:
                     bins.append(Bin(self.width, self.height))
 
-                    # Populate the itemsInBins dictionary based on the solution
-                    for t in T:
+                    # Populate the items
+                    for t in range(ub):
                         if X[b, t].x > 0.5:
                             bins[b].items.append(t)
-
-                    print("Items in bin", b, ":", bins[b].items)
-
         else:
-            print("No optimal solution found.")
+            raise NonOptimalException('Indices optimsation failed')
 
         return bins
