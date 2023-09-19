1a9de1103af412eaee2d7b437ff6dae18dfb12ca
LachieCottrill <110030951+LachieCottrill@users.noreply.github.com>
2023-09-19 16:38:25 +1000

    Revert "Revert "box plot and single plot""
    This reverts commit 7c4f618b11c9e38ba710a9e3343250c9c5a71893.

--- binpacking/plot.py
+++ binpacking/plot.py
@@ -9,37 +9,56 @@ from binpacking.model import Item
 
 def plot_solution(width: int, height: int, sol: list[Dict[int, tuple[int, int]]], items: list[Item]):
 
-    # Bin width * number of bins used in the solution
     xlim = width*len(sol)
 
-    # scaling plot display size
-    fig = plt.figure(figsize=(xlim/6, height/2))
+    fig = plt.figure(figsize=(len(sol), height/len(sol)))
     ax = fig.add_subplot(111)
 
     ax.set_xlim((0, xlim))
     ax.set_ylim((0, height))
     ax.set_aspect('equal')
 
-
-    xtick_locations = np.arange(0, xlim + 1, 5)
-    bin_labels = [f"Bin {i+1}" for i in range(len(sol))]
-    xtick_labels = []
-
-    for i in range(0, len(sol)*2 - 1, 2):
-        xtick_labels.append(str(xtick_locations[i]))
-        xtick_labels.append(f"{xtick_locations[i+1]} \n {bin_labels[int(i/2)]}")
-    # adding bin labels
-    xtick_labels.append(str(xtick_locations[-1]))
-
-    plt.xticks(xtick_locations, xtick_labels)
-    plt.yticks(np.arange(0, height + 1, 2))
+    plt.xticks(np.arange(0, xlim + width, width))
+    plt.yticks([0, height])
     plt.title("Instance ... Bins")
 
     for j, bin in enumerate(sol):
         rect = list(bin.items())
         for i in range(len(rect)):
             index = rect[i][0]
-            x1, y1 = rect[i][1][0] + 10*j, rect[i][1][1]
+            x1, y1 = rect[i][1][0] + width*j, rect[i][1][1]
+            x2, y2 = x1 + items[index].width, y1 + items[index].height
+
+            color = matplotlib.colors.to_hex(
+                [1.0 - (x2 - x1) / width, 1.0 - (y2 - y1) /
+                 height, 1.0])
+
+            rectPlot = matplotlib.patches.Rectangle((x1, y1), x2 - x1, y2 - y1, color=color)
+            ax.add_patch(rectPlot)
+            ax.annotate(index, (x1 + ((x2 - x1) / 2.0), y1 + ((y2 - y1) / 2.0)), color='w', weight='bold',
+                        fontsize=8, ha='center', va='center')
+
+    plt.show()
+
+
+def plot_grid(width: int, height: int, sol: list[Dict[int, tuple[int, int]]], items: list[Item]):
+    continue
+
+
+def plot_single(width: int, height: int, sol: list[Dict[int, tuple[int, int]]], items: list[Item]):
+
+    for j, bin in enumerate(sol):
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
+        ax.set_xlim(0, width)
+        ax.set_ylim(0, height)
+        ax.set_aspect('equal')
+
+        rect = list(bin.items())
+        for i in range(len(rect)):
+            index = rect[i][0]
+            x1, y1 = rect[i][1][0], rect[i][1][1]
             x2, y2 = x1 + items[index].width, y1 + items[index].height
 
             color = matplotlib.colors.to_hex(
@@ -50,5 +69,6 @@ def plot_solution(width: int, height: int, sol: list[Dict[int, tuple[int, int]]]
             ax.add_patch(rectPlot)
             ax.annotate(index, (x1 + ((x2 - x1) / 2.0), y1 + ((y2 - y1) / 2.0)), color='w', weight='bold',
                         fontsize=10, ha='center', va='center')
-
+        
+        plt.title(f"Bin {j + 1}")
     plt.show()
