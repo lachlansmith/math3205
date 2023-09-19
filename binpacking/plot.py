f1379319733b34bfd7e9fb78c3b285bb2711849e
LachieCottrill <110030951+LachieCottrill@users.noreply.github.com>
2023-09-19 12:44:50 +1000

    boxplot cleanup

--- binpacking/plot.py
+++ binpacking/plot.py
@@ -8,18 +8,32 @@ from binpacking.model import Item
 
 
 def plot_solution(width: int, height: int, sol: list[Dict[int, tuple[int, int]]], items: list[Item]):
-    """[ { 5: (0,0), 17: (0,7) }, {} ] -> [ [ (0,0), (10,7), #FF0000 ], [ (0,7), (10,3), #00FF00] ]"""
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
 
     # Bin width * number of bins used in the solution
-    xlim = height*len(sol)
+    xlim = width*len(sol)
+
+    # scaling plot display size
+    fig = plt.figure(figsize=(xlim/6, height/2))
+    ax = fig.add_subplot(111)
 
     ax.set_xlim((0, xlim))
     ax.set_ylim((0, height))
     ax.set_aspect('equal')
-    plt.xticks(np.arange(0, xlim, 2))
+
+
+    xtick_locations = np.arange(0, xlim + 1, 5)
+    bin_labels = [f"Bin {i+1}" for i in range(len(sol))]
+    xtick_labels = []
+
+    for i in range(0, len(sol)*2 - 1, 2):
+        xtick_labels.append(str(xtick_locations[i]))
+        xtick_labels.append(f"{xtick_locations[i+1]} \n {bin_labels[int(i/2)]}")
+    # adding bin labels
+    xtick_labels.append(str(xtick_locations[-1]))
+
+    plt.xticks(xtick_locations, xtick_labels)
+    plt.yticks(np.arange(0, height + 1, 2))
+    plt.title("Instance ... Bins")
 
     for j, bin in enumerate(sol):
         rect = list(bin.items())
@@ -35,6 +49,6 @@ def plot_solution(width: int, height: int, sol: list[Dict[int, tuple[int, int]]]
             rectPlot = matplotlib.patches.Rectangle((x1, y1), x2 - x1, y2 - y1, color=color)
             ax.add_patch(rectPlot)
             ax.annotate(index, (x1 + ((x2 - x1) / 2.0), y1 + ((y2 - y1) / 2.0)), color='w', weight='bold',
-                        fontsize=6, ha='center', va='center')
+                        fontsize=10, ha='center', va='center')
 
     plt.show()
